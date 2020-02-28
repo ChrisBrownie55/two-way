b4141e0232800781bb2e9d74cec6f88355c18f7c
Christopher Brown <chbphone55@gmail.com>
2020-02-27 20:39:14 -0600

    switch to .todo

--- index.spec.ts
+++ index.spec.ts
@@ -2,14 +2,9 @@ import '@testing-library/jest-dom/extend-expect';
 import { bind, subscribe } from './index.ts';
 
 describe.skip(`bind class property to a dom element's attributes/properties`, () => {
-  test('updates dom attributes', () => {
-  });
-  test('updates dom properties', () => {
-  });
-  test('only updates dom attributes/properties that are bound with `data-bind:<prop-name>`', () => {
-  });
+  test.todo('updates dom attributes');
+  test.todo('updates dom properties');
+  test.todo('only updates dom attributes/properties that are bound with `data-bind:<prop-name>`');
 });
 
-describe.skip('subscribe', () => {
-
-});
+describe.todo('subscribe');
