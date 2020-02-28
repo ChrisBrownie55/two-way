b6ec1e6e7fe6aa2a8498eb6f30ff5c164eb56707
Christopher Brown <chbphone55@gmail.com>
2020-02-27 22:03:38 -0600

    add class property change tests

--- index.spec.ts
+++ index.spec.ts
@@ -5,6 +5,12 @@ describe.skip(`bind class property to a dom element's attributes/properties`, ()
   test.todo('updates dom attributes');
   test.todo('updates dom properties');
   test.todo('only updates dom attributes/properties that are bound with `data-bind:<prop-name>`');
+
+  describe('updates class properties when dom event fires', () => {
+    test.todo('value');
+    test.todo('checked');
+    test.todo('custom');
+  });
 });
 
 describe.skip('subscribe', () => {});
