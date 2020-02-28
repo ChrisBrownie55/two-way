fc6585d452e656dbc444f3f825c3c713a8687448
Christopher Brown <chbphone55@gmail.com>
2020-02-27 20:30:21 -0600

    starting to write tests

---
+++ index.spec.ts
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/extend-expect';
+import { bind, subscribe } from './index.ts';
+
+describe.skip('bind class property to a dom element's attributes/properties', () => {
+  test('updates dom attributes', () => {
+  });
+  test('updates dom properties', () => {
+  });
+  test('only updates dom attributes/properties that are bound with `data-bind:<prop-name>`', () => {
+  });
+});
+
+describe.skip('subscribe', () => {
+
+});
