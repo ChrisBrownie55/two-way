b54185247b8811aa18e84c1d0c56b9b6878ab305
Christopher Brown <chbphone55@gmail.com>
2020-03-07 19:37:08 -0600

    add radio and checkbox values

--- test/index.ts
+++ test/index.ts
@@ -22,9 +22,10 @@ class TestBidirectional extends TestElement {
   checkedNames: string[] = [];
   uniqueNames: Set<string> = new Set;
   
-  radioSelected: string = '';
+  radioPicked: string = '';
 
-  
+  selected: string = '';
+  multiSelected: string[] = [];
  
   render() {
     this.#shadowRoot.innerHTML = `
@@ -35,14 +36,15 @@ class TestBidirectional extends TestElement {
       <input data-testid="individual-checkbox" type="checkbox" data-model="isChecked">
       
       <!-- Multiple Checkboxes (checked property filtered into & out of Array) -->
-      <input data-testid="checkbox-A1" type="checkbox" data-model="checkedNames">
-      <input data-testid="checkbox-A2" type="checkbox" data-model="checkedNames">
+      <input data-testid="checkbox-A1" type="checkbox" value="Brown" data-model="checkedNames">
+      <input data-testid="checkbox-A2" type="checkbox" value="Boring" data-model="checkedNames">
 
       <!-- Multiple Checkboxes (checked property added to or removed from Set) -->
-      <input data-testid="checkbox-S1" type="checkbox" data-model="uniqueNames">
-      <input data-testid="checkbox-S2" type="checkbox" data-model="uniqueNames">
+      <input data-testid="checkbox-S1" type="checkbox" value="Joe" data-model="uniqueNames">
+      <input data-testid="checkbox-S2" type="checkbox" value="Chris" data-model="uniqueNames">
 
-      
+      <input data-testid="radio-1" type="radio" value="One" data-model="radioPicked">
+      <input data-testid="radio-2" type="radio" value="Two" data-model="radioPicked">
     `;
   }
 }
