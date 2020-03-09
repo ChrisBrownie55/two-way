35e5414470a60d2c04e47ac69f85f6a75b08ec71
Christopher Brown <chbphone55@gmail.com>
2020-03-08 19:13:54 -0500

    customElements.define & jsdom-global install

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,25 @@
 import tap from 'tap';
 import '../mock/window.ts';
+
+import 
 import { bind } from '../index';
 
 class TestElement extends HTMLElement {
+  shadowRoot: ShadowRoot;
+  render: any;
+ 
   constructor() {
     super();
-    this.#shadowRoot = this.attachShadow({ mode: 'open' });
+    this.shadowRoot = this.attachShadow({ mode: 'open' });
     
     if (typeof this.render === 'function') {
       this.render();
     }
   }
+}
+
+class TestChild extends HTMLElement {
+  value = '';
 }
 
 @bind
@@ -19,16 +28,16 @@ class TestBidirectional extends TestElement {
 
   isChecked: boolean = false;
 
-  checkedNames: string[] = [];
-  uniqueNames: Set<string> = new Set;
+  checkedNames: string[] = ['Brown'];
+  uniqueNames: Set<string> = new Set('Joe');
   
-  radioPicked: string = '';
+  radioPicked: string = 'One';
 
-  selected: string = '';
-  multiSelected: string[] = [];
+  selected: string = 'A';
+  multiSelected: string[] = ['A'];
  
-  render() {
-    this.#shadowRoot.innerHTML = `
+  render = () => {
+    this.shadowRoot.innerHTML = `
       <!-- Text Input (value property)
       <input data-testid="text-input" type="text" data-model="value">
       
@@ -43,31 +52,45 @@ class TestBidirectional extends TestElement {
       <input data-testid="checkbox-S1" type="checkbox" value="Joe" data-model="uniqueNames">
       <input data-testid="checkbox-S2" type="checkbox" value="Chris" data-model="uniqueNames">
 
+      <!-- Radio Inputs (last one selected is set to active, all other are -->
       <input data-testid="radio-1" type="radio" value="One" data-model="radioPicked">
       <input data-testid="radio-2" type="radio" value="Two" data-model="radioPicked">
+
+      <select data-testid="select" data-model="selected">
+        <option>A</option>
+        <option>B</option>
+      </select>
+
+      <select data-testid="multi-select" data-model="multiSelected">
+        <option>A</option>
+        <option>B</option>
+      </select>
     `;
   }
 }
+customElements.define('test-bidirectional', TestBidirectional);
 
 @bind
 class TestUnidirectional extends TestElement {
   isChecked = false;
 
   count = 0;
-  handleClick = () => ++count;
+  handleClick = () => ++this.count;
 
-  render() {
-    this.#shadowRoot.innerHTML = `
+  render = () => {
+    this.shadowRoot.innerHTML = `
       <input data-testid="unidirectional" data-bind:checked="isChecked">
       <button data-testid="unidirectional-reverse" data-on:click="handleClick">Increment</button>
     `;
   }
 }
+customElements.define('test-unidirectional', TestUnidirectional);
 
-// bind class property to a dom element's attributes/properties
-  // updates dom attributes
+tap.test(`bind class property to a dom element's attributes/properties`, () => {
+  // updates dom attribute
   // updates dom properties
   // only updates dom attributes/properties that are bound with `data-model`
+});
 
   // updates class properties when dom event fires
     // value
